06e51c439f2be20999b0c9081c37feca79a30848
liu-dongyu <liudongyu.mail@gmail.com>
2016-09-17 00:27:35 +0800

    First commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import Config, { environment } from 'webpack-config';
+
+environment.setAll({
+  env: () => process.env.NODE_ENV
+});
+
+export default new Config().extend('./conf/webpack.[env].config.babel.js');
